2e790c8682954a5e9acb49c44991cfe2d30b1000
Mirlan Urzhanov <urzhanovmirlan@gmail.com>
2025-05-14 10:47:31 +0600

    feat: add product-list

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,5 +1,16 @@
-const HomePage = () => {
-  return <>Latest Products</>;
-};
+import ProductList from "@/components/shared/product/product-list";
+import sampleData from "@/db/sample-data";
 
+const HomePage = () => {
+  return (
+    <div className="space-y-8">
+      <h2 className="h2-bold">Latest Products</h2>
+      <ProductList
+        title="Newest Arrivals"
+        data={sampleData.products}
+        limit={4}
+      />
+    </div>
+  );
+};
 export default HomePage;
---
+++ components/shared/product/product-list.tsx
@@ -0,0 +1,31 @@
+const ProductList = ({
+  data,
+  title,
+  limit,
+}: {
+  data: any[];
+  title?: string;
+  limit?: number;
+}) => {
+  // Apply limit if provided, otherwise show all products
+  const limitedData = limit ? data.slice(0, limit) : data;
+
+  return (
+    <div className="my-10">
+      <h2 className="h2-bold mb-4">{title}</h2>
+      {limitedData.length > 0 ? (
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+          {limitedData.map((product: any) => (
+            <>{product.name}</>
+          ))}
+        </div>
+      ) : (
+        <div>
+          <p>No products found</p>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default ProductList;
