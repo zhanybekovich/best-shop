1568a8f13badef0eac6727d4f66d59f08b7122f4
Mirlan Urzhanov <urzhanovmirlan@gmail.com>
2025-05-15 21:21:00 +0600

    feat: add getLatestProducts action

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,16 +1,15 @@
 import ProductList from "@/components/shared/product/product-list";
-import sampleData from "@/db/sample-data";
+import { getLatestProducts } from "@/lib/actions/product.actions";
 
-const HomePage = () => {
+const HomePage = async () => {
+  const latestProducts = await getLatestProducts();
+
   return (
     <div className="space-y-8">
       <h2 className="h2-bold">Latest Products</h2>
-      <ProductList
-        title="Newest Arrivals"
-        data={sampleData.products}
-        limit={4}
-      />
+      <ProductList title="Newest Arrivals" data={latestProducts} />
     </div>
   );
 };
+
 export default HomePage;
--- lib/constants/index.ts
+++ lib/constants/index.ts
@@ -4,3 +4,6 @@ export const APP_DESCRIPTION =
   "A modern store built with Next.js";
 export const SERVER_URL =
   process.env.NEXT_PUBLIC_SERVER_URL || "http://localhost:3000";
+
+export const LATEST_PRODUCTS_LIMIT =
+  Number(process.env.LATEST_PRODUCTS_LIMIT) || 4;
