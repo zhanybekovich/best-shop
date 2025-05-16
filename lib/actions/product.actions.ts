d32479ccef1213f04eca91d8fa2c55de1c19ee5f
Mirlan Urzhanov <urzhanovmirlan@gmail.com>
2025-05-16 13:18:45 +0600

    feat: add product page

--- lib/actions/product.actions.ts
+++ lib/actions/product.actions.ts
@@ -11,3 +11,9 @@ export async function getLatestProducts() {
 
   return convertToPlainObject(data);
 }
+
+export async function getProductBySlug(slug: string) {
+  return await prisma.product.findFirst({
+    where: { slug: slug },
+  });
+}
